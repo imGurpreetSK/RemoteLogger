905454d945322c89e8c3124f73ff787d412aef9f
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-11-23 12:37:52 +0530

    RemoteLog, create data class.

---
+++ remotelogger/src/main/java/com/gurpreetsk/remotelogger/RemoteLog.kt
@@ -0,0 +1,14 @@
+package com.gurpreetsk.remotelogger
+
+data class RemoteLog(
+    val id: Int,
+    val userUUID: String,
+    val timestamp: Long,
+    val osName: String,
+    val osVersion: String,
+    val appVersion: String,
+    val logTag: String,
+    val logLevel: String,
+    val message: String,
+    val stackTrace: String
+)
