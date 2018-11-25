2c4488517a658cb50e9dae7a7f84fea1eb870eb3
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-11-25 12:00:20 +0530

    RemoteJobExecutor, working network requests, minor cleanup.

--- remotelogger/src/main/java/com/gurpreetsk/remotelogger/RemoteJobExecutor.kt
+++ remotelogger/src/main/java/com/gurpreetsk/remotelogger/RemoteJobExecutor.kt
@@ -1,12 +1,16 @@
 package com.gurpreetsk.remotelogger
 
 import com.gurpreetsk.remotelogger.internal.logError
+import com.gurpreetsk.remotelogger.internal.logInfo
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import org.json.JSONArray
 import java.io.BufferedOutputStream
+import java.io.BufferedReader
 import java.io.BufferedWriter
+import java.io.IOException
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
@@ -21,8 +25,7 @@ object RemoteJobExecutor {
       GlobalScope.launch {
         val logs = async { storageType.getLogs() }.await()
         if (logs.isNotEmpty()) {
-          makeRequest(url, logs)
-              .also { storageType.purge() }
+          pushLogsToServer(url, logs) { storageType.purge() }
         }
       }
     } catch (e: Exception) {
@@ -30,22 +33,25 @@ object RemoteJobExecutor {
     }
   }
 
-  private fun makeRequest(
+  @Throws(MalformedURLException::class, IOException::class)
+  private fun pushLogsToServer(
       remoteUrl: String,
-      logs: List<RemoteLog>
+      logs: List<RemoteLog>,
+      purge: () -> Int
   ) {
     if (remoteUrl.isBlank()) {
-      throw MalformedURLException("Blank url")
+      throw MalformedURLException()
     }
 
     // 1. Declare a URL Connection
-    val url  = URL(remoteUrl)
+    val url = URL(remoteUrl)
     val urlConnection = url.openConnection() as HttpURLConnection
+    urlConnection.requestMethod = "POST"
 
     // 2. Open InputStream to connection
     urlConnection.connect()
 
-    // 3. Download and decode the string response using builder
+    // 3. Make the POST request
     val outputStream = BufferedOutputStream(urlConnection.outputStream)
 
     val writer = BufferedWriter(OutputStreamWriter(outputStream, "UTF-8"))
@@ -55,5 +61,22 @@ object RemoteJobExecutor {
     outputStream.close()
 
     urlConnection.connect()
+
+    // 4. Get the response
+    val inputStream = BufferedReader(InputStreamReader(urlConnection.inputStream))
+
+    // Read Server Response
+    val sb = StringBuilder()
+    var readLine = inputStream.readLine()
+    while (readLine != null) {
+      sb.append(readLine + "\n")
+      readLine = inputStream.readLine()
+    }
+
+    logInfo("RemoteJobExecutor", sb.toString())
+
+    if (urlConnection.responseCode == 200) {
+      purge.invoke()
+    }
   }
 }
