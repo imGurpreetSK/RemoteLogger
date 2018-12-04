cccfade48c7dd8f55edc2c5241ae95cc65de0029
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-12-04 21:24:16 +0530

    MainActivity, update layout, add button to add logs.

--- app/src/main/java/com/gurpreetsk/remotelogger/MainActivity.kt
+++ app/src/main/java/com/gurpreetsk/remotelogger/MainActivity.kt
@@ -15,8 +15,10 @@
  */
 package com.gurpreetsk.remotelogger
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.addLogsButton
 import kotlinx.android.synthetic.main.main_activity.dummyTextView
 import java.lang.NullPointerException
 
@@ -24,15 +26,19 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_activity)
+
+    addLogsButton.setOnClickListener {
+      logAndUpdateDummyTextView()
+    }
   }
 
-  override fun onStart() {
-    super.onStart()
+  @SuppressLint("SetTextI18n") // Sample application.
+  fun logAndUpdateDummyTextView() {
     RemoteLogger.i("MainActivity", "Info log")
     RemoteLogger.d("MainActivity", "Debug log")
     RemoteLogger.v("MainActivity", "Verbose log")
     RemoteLogger.e("MainActivity", "Error log", NullPointerException("Expected behaviour"))
 
-    dummyTextView.text = "Logged"
+    dummyTextView.text = dummyTextView.text.toString() + "Logs inserted\n"
   }
 }
