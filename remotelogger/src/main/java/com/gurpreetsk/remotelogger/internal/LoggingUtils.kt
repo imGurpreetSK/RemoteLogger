b825d90f214daaed7c36783f73f7572a0eb7c0b7
Gurpreet Singh <gurpreetsk@hotmail.com>
2019-01-21 14:06:44 +0530

    LoggingUtils, temporarily log everything to console.

--- remotelogger/src/main/java/com/gurpreetsk/remotelogger/internal/LoggingUtils.kt
+++ remotelogger/src/main/java/com/gurpreetsk/remotelogger/internal/LoggingUtils.kt
@@ -16,16 +16,11 @@
 package com.gurpreetsk.remotelogger.internal
 
 import android.util.Log
-import com.gurpreetsk.remotelogger.BuildConfig
 
 fun logError(tag: String, message: String, throwable: Throwable?) {
-  if (BuildConfig.DEBUG) {
-    Log.e(tag, message, throwable)
-  }
+  Log.e(tag, message, throwable)
 }
 
 fun logInfo(tag: String, message: String) {
-  if (BuildConfig.DEBUG) {
-    Log.i(tag, message)
-  }
+  Log.i(tag, message)
 }
